6cd9c42058c8ab38d3d51bb9def8acee82a3bbb1
adiog <adiog@brainfuck.pl>
2018-09-12 10:28:36 +0200

    RFC: initial implementation

---
+++ source/sample.h
@@ -0,0 +1,24 @@
+// This file is a part of parser project.
+// Copyright 2018 Aleksander Gajewski <adiog@brainfuck.pl>.
+
+const char* sample =
+"CRANE ORIENTATION DYNAMIC "
+" DISTANCE DISPLACEMENT DYNAMIC "
+"  BACKPACK ORIENTATION DYNAMIC "
+"   LEFT_LEG_OFFSET DISPLACEMENT STATIC 1230.0 0.0 0.0 "
+"    LEFT_LEG_UPPER ORIENTATION DYNAMIC "
+"     LEFT_LEG_LOWER ORIENTATION DYNAMIC "
+"   RIGHT_LEG_OFFSET DISPLACEMENT STATIC 0.0 0.0 0.0 "
+"    RIGHT_LEG_UPPER ORIENTATION DYNAMIC "
+"     RIGHT_LEG_LOWER ORIENTATION DYNAMIC ";
+
+const char* sampleFrame =
+        "1 2 3 "
+        "4 5 6 "
+        "7 8 9 "
+        ""
+        "10 11 12 "
+        "13 14 15 "
+        ""
+        "16 17 18 "
+        "19 20 21 ";
