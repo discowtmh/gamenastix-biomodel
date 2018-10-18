bf17d784b9f5496e835456c5ad4c754a03e56f65
Aleksander Gajewski <adiog@brainfuck.pl>
2018-10-18 12:40:18 +0200

    adding description to models

--- include/deepModel/Bio4.h
+++ include/deepModel/Bio4.h
@@ -11,6 +11,30 @@
 namespace biomodel {
 namespace deepModel {
 
+/**
+ * Simplified Legs Model
+ *
+ * @brief Simplified Legs model for Treadmill 2.0
+ *
+ * Model has a following structure: \n
+ * \n
+ * PELVIS ORIENTATION DYNAMIC \n
+ * \ LEFT_LEG_OFFSET DISPLACEMENT STATIC 0.0 0.0 0.0 \n
+ * \ \ LEFT_LEG_UPPER ORIENTATION DYNAMIC \n
+ * \ \ \ LEFT_LEG_LOWER ORIENTATION DYNAMIC \n
+ * \ RIGHT_LEG_OFFSET DISPLACEMENT STATIC 0.0 0.0 0.0 \n
+ * \ \ RIGHT_LEG_UPPER ORIENTATION DYNAMIC \n
+ * \ \ \ RIGHT_LEG_LOWER ORIENTATION DYNAMIC \n
+ * \n
+ * Each frame contains the current orientation (euler yaw/pitch/roll angles)
+ * and displacement (3 dimensional relative offset) for each dynamic component:\n
+ * \n
+ * PELVIS\n
+ * LEFT_LEG_UPPER\n
+ * LEFT_LEG_LOWER\n
+ * RIGHT_LEG_UPPER\n
+ * RIGHT_LEG_LOWER
+ **/
 struct Bio4 : public Medium
 {
     Model getModel() const override
